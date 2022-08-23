244f93d48ef42a59c18dc31efbe085d7f1e0b3f8
vinhng2301 <ducvinhnguyen0201@gmail.com>
2022-08-23 13:59:31 +0700

    add service

---
+++ src/main/java/com/example/jdbc_th/service/IUserDao.java
@@ -0,0 +1,19 @@
+package com.example.jdbc_th.service;
+
+import com.example.jdbc_th.model.User;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface IUserDao {
+    public void insertUser(User users) throws SQLException;
+
+    public User selectUser(int id);
+
+    public List<User> selectAllUser();
+
+    public boolean deleteUser(int id) throws SQLException;
+
+    public boolean updateUser(User users) throws SQLException;
+
+}
